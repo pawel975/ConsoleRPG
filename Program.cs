20af157ea14b866a11cac0f9a4792be1ceb0cf04
Paweł Kurek <pawel.kurek975@gmail.com>
2023-10-29 16:28:19 +0100

    init game mechanism

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace ConsoleRPG
     {
         static public string saveFileDirectoryPath = default;
         static readonly private string saveFileName = "saveFile.json";
+        static public bool GameActive = true;
+        static public bool IsGameExit = false;
         // Init player instance
         static public Player player = default;
+
         static void LoadGame()
         {
             string serializedProgress = default;
@@ -38,11 +41,64 @@ namespace ConsoleRPG
 
         }
 
-        static void Main(string[] args)
+        static void NewGame()
         {
-            Console.Write("Insert save file directory path to load/save progress");
-            saveFileDirectoryPath = Console.ReadLine();
+            Console.Write("Insert character name: ");
+            string name = Console.ReadLine();
 
+            player = new Player(name, new List<Statistic>());
+        }
+
+        static int Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Insert save file directory path to load/save progress, [x - to exit]: ");
+                saveFileDirectoryPath = Console.ReadLine();
+
+                if (saveFileDirectoryPath == "x")
+                {
+                    return 0;
+                }
+            } while (!Directory.Exists(saveFileDirectoryPath));
+
+            do
+            {
+                Console.WriteLine("Choose action: ");
+                Console.WriteLine("1 - New Game ");
+                Console.WriteLine("2 - Load Game ");
+                Console.WriteLine("X - Exit");
+
+                string userPickedOption = Console.ReadLine();
+
+                switch (userPickedOption.ToLower())
+                {
+                    case "1":
+                        NewGame();
+                        GameActive = true;
+                        break;
+                    case "2":
+                        LoadGame();
+                        GameActive = true;
+                        break;
+                    case "x":
+                        LoadGame();
+                        IsGameExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option choice...");
+                        break;
+                }
+
+            } while (!GameActive || IsGameExit);
+
+            //TODO: Game until player choose [x]
+            do
+            {
+
+            } while (IsGameExit);
+
+            return 0;
         }
     }
 }
